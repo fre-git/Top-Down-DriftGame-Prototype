306609b4e1257ed8de7cf70955e7253de57fa370
fre-git <frederic.gielen8@gmail.com>
2024-06-30 17:44:37 +0200

    commited old project to share on github

---
+++ desktop/src/com/topdowncar/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.topdowncar.game.desktop;
+
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.topdowncar.game.CarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		new Lwjgl3Application(new CarGame(), config);
+	}
+}
